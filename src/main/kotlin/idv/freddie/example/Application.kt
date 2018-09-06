1e61020dcdb2275ae20705b6de8a811eeec4f50f
Freddie Wang <yunghsing.wang@linecorp.com>
2018-09-06 17:12:30 +0900

    Add logs in each point

--- src/main/kotlin/idv/freddie/example/Application.kt
+++ src/main/kotlin/idv/freddie/example/Application.kt
@@ -9,7 +9,7 @@ var continuation: Continuation<Int>? = null
 class SimpleCoroutine<T>(override val context: CoroutineContext) : Continuation<T> {
 
     override fun resume(value: T) {
-        log("resume in SimpleCoroutine")
+        log("[POINT12] resume in SimpleCoroutine2")
     }
 
     override fun resumeWithException(exception: Throwable) {
@@ -21,40 +21,40 @@ class SimpleCoroutine<T>(override val context: CoroutineContext) : Continuation<
     }
 }
 
-fun main(args: Array<String>) {// = runBlocking {
+fun main(args: Array<String>) {
 
     // Start a coroutine
-    val test = SimpleCoroutine<Unit>(EmptyCoroutineContext).startCoroutine {
-        log("in launch")
+    SimpleCoroutine<Unit>(EmptyCoroutineContext).startCoroutine {
+        log("[POINT1] Before invoking a()")
         val a = a()
-        log("Result is $a")
+        log("[POINT2] Result is $a")
     }
 
-    10.downTo(0).forEach {
-        log("downTo $it")
+    5.downTo(0).forEach {
+        log("[POINT3] it: $it")
         continuation?.resume(it)
     }
-    log("Leaving main()")
+    log("[POINT4] Leaving main()")
 }
 
 suspend fun a(): Int {
-    log("running a()")
+    log("[POINT5] entering a")
     val result = b()
-    log("leaving a()")
+    log("[POINT6] leaving b")
     return result
 }
 
 suspend fun b(): Int {
-    log("running b()")
+    log("[POINT7] entering b()")
     while (true) {
-        log("before suspend")
+        log("[POINT8] before suspend")
         val i: Int = suspendCoroutine { cont ->
-            log("assign continuation")
+            log("[POINT9] assign continuation")
             continuation = cont
         }
-        log("After suspend, i = $i")
+        log("[POINT10] After suspend, i = $i")
         if (i == 0) {
-            log("leaving b()")
+            log("[POINT11] leaving b()")
             return 0
         }
     }
