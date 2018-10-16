d84629226cc67e5394bb9f4440d71af2c10527a5
Freddie Wang <wangyung@gmail.com>
2018-10-16 18:12:16 +0900

    Separate the demos into example1 and example2

--- src/main/kotlin/idv/freddie/example/Application.kt
+++ src/main/kotlin/idv/freddie/example/Application.kt
@@ -1,66 +1,6 @@
 package idv.freddie.example
 
-import java.util.concurrent.ThreadLocalRandom
-import kotlin.concurrent.thread
-import kotlin.coroutines.experimental.*
-import kotlin.math.abs
-
-fun log(message: String) = println("[${Thread.currentThread().name}] $message")
-
-var continuation: Continuation<Int>? = null
-
-val useThread = false
-
 fun main(args: Array<String>) {
-
-    // Start a coroutine
-    SimpleCoroutine<Unit>().startCoroutine {
-        log("[POINT1] Before invoking a()")
-        val resultA = a()
-        log("[POINT2] Result is $resultA")
-    }
-
-    if (!useThread) {
-        (4990..5010).forEach {
-            continuation?.resume(it) ?: return@forEach
-        }
-    }
-
-    log("[POINT3] Leaving main()")
-}
-
-suspend fun a(): Int {
-    log("[POINT4] entering b in a")
-    val result = b()
-    log("[POINT5] leaving b in a")
-    return result
-}
-
-suspend fun b(): Int {
-    log("[POINT6] entering b()")
-    var counter = 0
-    while (true) {
-        log("[POINT7] before suspend")
-        val i: Int = suspendCoroutine { cont ->
-            log("[POINT8] in suspendCoroutine")
-            if (useThread) {
-                thread {
-                    val sleepTime = abs(ThreadLocalRandom.current().nextLong()) % 1000
-                    Thread.sleep(sleepTime)
-                    log("[POINT9] before resume")
-                    cont.resume(sleepTime.toInt())
-                    log("[POINT10] leaving thread")
-                }
-            } else {
-                continuation = cont
-            }
-        }
-        log("[POINT11] After suspend, i = $i")
-        counter += i
-        if (counter > 5000) {
-            log("[POINT12] leaving b()")
-            continuation = null
-            return counter
-        }
-    }
+    runExample1()
+    runExample2()
 }
---
+++ src/main/kotlin/idv/freddie/example/Example1.kt
@@ -0,0 +1,53 @@
+package idv.freddie.example
+
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
+
+fun runExample1() = runBlocking {
+    val threadRunningTime = useThread()
+    val coroutineRunningTime = useCoroutines()
+
+    print(
+            "using $threadRunningTime ms in threads\n" +
+            "using $coroutineRunningTime ms in coroutines\n"
+    )
+}
+
+fun useThread(): Long =
+        measureTimeMillis {
+            val threadMap = mutableMapOf<String, Int>()
+            val jobs = List(10000) {
+                try {
+                    thread {
+                        Thread.sleep(1000)
+                        if (!threadMap.containsKey(Thread.currentThread().name)) {
+                            threadMap[Thread.currentThread().name] = it
+                        }
+                        print(".")
+                    }
+                } catch (e: OutOfMemoryError) {
+                    print("[OOM] oops!\n")
+                }
+            }
+            jobs.forEach { (it as? Thread)?.join() }
+            print("\nThe number of threads: ${threadMap.size}\n")
+        }
+
+suspend fun useCoroutines(): Long =
+        measureTimeMillis {
+            val threadMap = mutableMapOf<String, Int>()
+            val jobs = List(10000) {
+                launch {
+                    delay(1000)
+                    if (!threadMap.containsKey(Thread.currentThread().name)) {
+                        threadMap[Thread.currentThread().name] = it
+                    }
+                    print(".")
+                }
+            }
+            jobs.forEach { it.join() }
+            print("\nThe number of threads of coroutines: ${threadMap.size}\n")
+        }
